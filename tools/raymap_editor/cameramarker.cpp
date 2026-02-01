219d1d5d02f1170cc14b40791cd09ad431dd7e57
Rufidj <divaio19@gmail.com>
2026-02-01 18:45:03 +0100

    Delete tools/raymap_editor directory

--- tools/raymap_editor/cameramarker.cpp
+++
@@ -1,8 +0,0 @@
-#include "cameramarker.h"
-
-CameraMarker::CameraMarker(QWidget *parent)
-    : QWidget(parent)
-    , m_mapData(nullptr)
-{
-    // TODO: Implementar marcador de cámara
-}
--- tools/raymap_editor/spriteeditor.cpp
+++
@@ -1,8 +0,0 @@
-#include "spriteeditor.h"
-
-SpriteEditor::SpriteEditor(QWidget *parent)
-    : QWidget(parent)
-    , m_mapData(nullptr)
-{
-    // TODO: Implementar editor de sprites
-}
